86768d95596cf8cfce4930bc4429bf1cd92ab116
stanleyedward <stanleyedward9715@gmail.com>
2024-01-23 13:49:29 +0530

    finalize recursion again

--- panorama.py
+++ panorama.py
@@ -4,9 +4,13 @@ import sys
 import cv2
 
 def main(image_dir_list):
+    
     images_list, no_of_images = read_images.read(image_dir_list)
     result = recursion.recurse(images_list, no_of_images)
     cv2.imwrite("outputs/panorama_image.jpg", result)
     
 if __name__=='__main__':
-    main([sys.argv[1], sys.argv[2]])
+    image_list = []
+    for i in range(1,len(sys.argv)):
+        image_list.append(sys.argv[i])
+    main(image_list)
