3cf6414f003d98b2e01bcd9b6e317eccb6575fac
stanleyedward <stanleyedward9715@gmail.com>
2024-01-23 15:11:59 +0530

    init doc

--- image_stitching/__init__.py
+++ image_stitching/__init__.py
@@ -1,5 +1,6 @@
 import pyfiglet
-
+"""Initialization for the script and displays figlet text
+"""
 panorama_text = pyfiglet.figlet_format("Panorama") 
 print(panorama_text) 
 print(f"Initializing...")
