7eab432b1e05b44f78430d03de9983d8a4a6dc67
stanleyedward <stanleyedward9715@gmail.com>
2024-01-23 12:57:37 +0530

    mod recursion

--- recursion.py
+++ recursion.py
@@ -1,12 +1,12 @@
-import panorama
+import utils
 
 def recurse(image_list, len_of_list):
     if len_of_list == 2:
-        result = panorama.forward(query_photo=image_list[len_of_list-2], train_photo=image_list[len_of_list-1])
+        result = utils.forward(query_photo=image_list[len_of_list-2], train_photo=image_list[len_of_list-1])
         
         return result
     else:
-        result = panorama.forward(query_photo=image_list[len_of_list-2], train_photo=image_list[len_of_list-1])
+        result = utils.forward(query_photo=image_list[len_of_list-2], train_photo=image_list[len_of_list-1])
         image_list[len_of_list-2] = result
         
         return recurse(image_list, len_of_list-1)
