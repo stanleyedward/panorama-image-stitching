00a900ed01c563907996ea58118f9458f1631836
stanleyedward <stanleyedward9715@gmail.com>
2024-01-23 15:27:58 +0530

    end msg

--- panorama.py
+++ panorama.py
@@ -7,7 +7,7 @@ import cv2
 def main(image_dir_list):
     """Main function of the Repository.
     Takes in list of image dir, runs the complete pipeline 
-    to create and export a panoramic image
+    to create and export a panoramic image in the /outputs/ folder.
 
     Args:
         image_dir_list (List): List of image dirs passed in cmdline
@@ -16,6 +16,8 @@ def main(image_dir_list):
     images_list, no_of_images = read_images.read(image_dir_list)
     result = recursion.recurse(images_list, no_of_images)
     cv2.imwrite("outputs/panorama_image.jpg", result)
+    
+    print(f"Panoramic image saved at: outputs/panorama_image.jpg")
 
 
 if __name__ == "__main__":
