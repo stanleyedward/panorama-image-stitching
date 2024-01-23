10c7f55971e644c8371aca70b8b3a9692bc51f9e
stanleyedward <stanleyedward9715@gmail.com>
2024-01-23 11:27:37 +0530

    bug fixes

--- image_stitching.py
+++ image_stitching.py
@@ -1,86 +1,108 @@
 import numpy as np
 import cv2
 
-class ImageStitching():
+
+class ImageStitching:
     def __init__(self):
         super().__init__()
         self.smoothing_window_size = 800
-    
+
     def read_images(self, image):
         photo = cv2.imread(image)
         photo = cv2.cvtColor(photo, cv2.COLOR_BGR2RGB)
         photo_gray = cv2.cvtColor(photo, cv2.COLOR_RGB2GRAY)
-        
+
         return photo, photo_gray
-    
+
     @staticmethod
     def _sift_detector(image):
         descriptor = cv2.SIFT_create()
         keypoints, features = descriptor.detectAndCompute(image, None)
-        
+
         return keypoints, features
-        
+
     def create_and_match_keypoints(self, features_train_image, features_query_image):
         bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
-        
+
         best_matches = bf.match(features_train_image, features_query_image)
-        raw_matches = sorted(best_matches, key= lambda x: x.distance)
-        
+        raw_matches = sorted(best_matches, key=lambda x: x.distance)
+
         return raw_matches
-    
-    def compute_homography(self, keypoints_train_image, keypoints_query_image, matches, reprojThresh):
-        keypoints_train_image = np.float32([keypoint.pt for keypoint in keypoints_train_image])
-        keypoints_query_image = np.float32([keypoint.pt for keypoint in keypoints_query_image])
-        
+
+    def compute_homography(
+        self, keypoints_train_image, keypoints_query_image, matches, reprojThresh
+    ):
+        keypoints_train_image = np.float32(
+            [keypoint.pt for keypoint in keypoints_train_image]
+        )
+        keypoints_query_image = np.float32(
+            [keypoint.pt for keypoint in keypoints_query_image]
+        )
+
         if len(matches) >= 4:
-            points_train = np.float32([keypoints_train_image[m.queryIdx] for m in matches])
-            points_query = np.float32([keypoints_query_image[m.trainIdx] for m in matches])
-            
-            H, status = cv2.findHomography(points_train, points_query, cv2.RANSAC, reprojThresh)
-        
+            points_train = np.float32(
+                [keypoints_train_image[m.queryIdx] for m in matches]
+            )
+            points_query = np.float32(
+                [keypoints_query_image[m.trainIdx] for m in matches]
+            )
+
+            H, status = cv2.findHomography(
+                points_train, points_query, cv2.RANSAC, reprojThresh
+            )
+
             return (matches, H, status)
-    
-        else: 
+
+        else:
             print(f"Minimum match count not satisfied cannot get homopgrahy")
             return None
-        
-    def create_mask(self,query_image, train_image, version):
+
+    def create_mask(self, query_image, train_image, version):
         height_query_photo = query_image.shape[0]
         width_query_photo = query_image.shape[1]
         width_train_photo = train_image.shape[1]
         height_panorama = height_query_photo
-        width_panorama = width_query_photo +width_train_photo
+        width_panorama = width_query_photo + width_train_photo
         offset = int(self.smoothing_window_size / 2)
         barrier = query_image.shape[1] - int(self.smoothing_window_size / 2)
         mask = np.zeros((height_panorama, width_panorama))
-        if version == 'left_image':
-            mask[:, barrier - offset:barrier + offset ] = np.tile(np.linspace(1, 0, 2 * offset ).T, (height_panorama, 1))
-            mask[:, :barrier - offset] = 1
+        if version == "left_image":
+            mask[:, barrier - offset : barrier + offset] = np.tile(
+                np.linspace(1, 0, 2 * offset).T, (height_panorama, 1)
+            )
+            mask[:, : barrier - offset] = 1
         else:
-            mask[:, barrier - offset :barrier + offset ] = np.tile(np.linspace(0, 1, 2 * offset ).T, (height_panorama, 1))
-            mask[:, barrier + offset:] = 1
+            mask[:, barrier - offset : barrier + offset] = np.tile(
+                np.linspace(0, 1, 2 * offset).T, (height_panorama, 1)
+            )
+            mask[:, barrier + offset :] = 1
         return cv2.merge([mask, mask, mask])
-    
+
     def blending_smoothing(self, query_image, train_image, homography_matrix):
         height_img1 = query_image.shape[0]
         width_img1 = query_image.shape[1]
         width_img2 = train_image.shape[1]
         height_panorama = height_img1
-        width_panorama = width_img1 +width_img2
-        
+        width_panorama = width_img1 + width_img2
+
         panorama1 = np.zeros((height_panorama, width_panorama, 3))
-        mask1 = self.create_mask(query_image, train_image, version='left_image')
-        panorama1[0:query_image.shape[0], 0:query_image.shape[1], :] = query_image
+        mask1 = self.create_mask(query_image, train_image, version="left_image")
+        panorama1[0 : query_image.shape[0], 0 : query_image.shape[1], :] = query_image
         panorama1 *= mask1
-        mask2 = self.create_mask(query_image, train_image, version='right_image')
-        panorama2 = cv2.warpPerspective(train_image, homography_matrix, (width_panorama, height_panorama))*mask2
-        result=panorama1+panorama2
-        
-        #remove extra blackspace
+        mask2 = self.create_mask(query_image, train_image, version="right_image")
+        panorama2 = (
+            cv2.warpPerspective(
+                train_image, homography_matrix, (width_panorama, height_panorama)
+            )
+            * mask2
+        )
+        result = panorama1 + panorama2
+
+        # remove extra blackspace
         rows, cols = np.where(result[:, :, 0] != 0)
         min_row, max_row = min(rows), max(rows) + 1
         min_col, max_col = min(cols), max(cols) + 1
-        
+
         final_result = result[min_row:max_row, min_col:max_col, :]
-        
-        return final_result 
+
+        return final_result
--- panorama.py
+++ panorama.py
@@ -1,41 +1,52 @@
 import cv2
 from image_stitching import ImageStitching
 import sys
+import numpy as np
+
 
 def forward(query_image_dir, train_image_dir):
     image_stitching = ImageStitching()
-    query_photo, query_photo_gray = image_stitching.read_images('inputs/desk/pa1.jpg') #left image
-    train_photo, train_photo_gray = image_stitching.read_images('inputs/desk/pa2.jpg') #right image
-    
-    
-    keypoints_train_image, features_train_image = image_stitching._sift_detector(train_photo_gray) 
-    keypoints_query_image, features_query_image = image_stitching._sift_detector(query_photo_gray) 
-    
-    matches = image_stitching.create_and_match_keypoints(features_train_image, features_query_image)
-    
-    M = image_stitching.compute_homography(keypoints_train_image, keypoints_query_image, matches, reprojThresh=4)
-    
-    if M is None: 
-        print(f"Error")
-        
-    (matches, homography_matrix, status) = M
-    
-    result = image_stitching.blending_smoothing(query_photo, train_photo, homography_matrix)
-    mapped_image = cv2.drawMatches(train_photo, keypoints_train_image, query_photo, keypoints_query_image, matches[:100], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    
-    # cv2.imshow("keypoint matches", mapped_image)
-    # cv2.imshow("panorama", result)
-    
-    cv2.imwrite("outputs/matched_points.jpg", mapped_image)
-    cv2.imwrite("outputs/panorama_image.jpg", result)
-    
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    query_photo, query_photo_gray = image_stitching.read_images(
+        query_image_dir
+    )  # left image
+    train_photo, train_photo_gray = image_stitching.read_images(
+        train_image_dir
+    )  # right image
 
-if __name__=='__main__':
+    keypoints_train_image, features_train_image = image_stitching._sift_detector(
+        train_photo_gray
+    )
+    keypoints_query_image, features_query_image = image_stitching._sift_detector(
+        query_photo_gray
+    )
+
+    matches = image_stitching.create_and_match_keypoints(
+        features_train_image, features_query_image
+    )
+
+    M = image_stitching.compute_homography(
+        keypoints_train_image, keypoints_query_image, matches, reprojThresh=4
+    )
+
+    if M is None:
+        print(f"Error")
+
+    (matches, homography_matrix, status) = M
+
+    result = image_stitching.blending_smoothing(
+        query_photo, train_photo, homography_matrix
+    )
+    # mapped_image = cv2.drawMatches(train_photo, keypoints_train_image, query_photo, keypoints_query_image, matches[:100], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    result_float32 = np.float32(result)
+    result_rgb = cv2.cvtColor(result_float32, cv2.COLOR_BGR2RGB)
+    
+    return result_rgb
+
+
+if __name__ == "__main__":
     try:
-        # forward(query_image=sys.argv[1], train_image= sys.argv[2])
-        forward()
+        result = forward(query_image_dir=sys.argv[1], train_image_dir=sys.argv[2])
+        cv2.imwrite("outputs/panorama_image.jpg", result)
     except IndexError:
-        print ("Please input atleast two source images")
-        
+        print("Please input atleast two source images")
